59019c0efe4be6da220058150fc3746cbb74f705
Firdavs375 <firdavss.3755@gmail.com>
2023-03-27 16:05:06 +0500

    flex

---
+++ index.js
@@ -0,0 +1,23 @@
+// const son = document.getElementById("inp")
+// let son2 = perseFloat(son.value);
+// function www() {
+//     var num1 = 50,
+//         num2 = 50,
+//         num3 = 50,
+//         num4 = 50;
+//     var res = num1 + num2 + num3 + num4;
+
+
+//     if (son2 <= res) {
+//         document.write("Pul yetarli")
+//     } else {
+//         document.write("Pul yetarli emas")
+//     }
+
+
+
+
+//     // document.getElementById("javob").innerHTML = res;
+
+
+// }
